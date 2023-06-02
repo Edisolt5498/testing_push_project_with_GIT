716e7660a20e07759da6bc90477f66e9a4e0b584
Edisolt Khatsiev <edisolt5498@gmail.com>
2023-06-02 21:43:43 +0300

    just changet name of Scanner

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner inputNumberOfLines = new Scanner(System.in);
 
         System.out.print("Input a number of lines of arrow: ");
-        int n = scanner.nextInt;
+        int n = inputNumberOfLines.nextInt;
 
         //int n = 12; //the size of arrow
         int last = n*2-1; // num of symbols at last line
